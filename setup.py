8c3d430b9bbe4cd0ec9e14ed85f060443adc8c3e
Roland Hedberg <roland@catalogix.se>
2019-03-12 09:37:55 +0100

    Bumped versions

--- setup.py
+++ setup.py
@@ -64,9 +64,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     install_requires=[
-        'cryptojwt>=0.6.6',
-        'oidcservice>=0.6.2',
-        'oidcmsg>=0.6.2'
+        'cryptojwt>=0.7.0',
+        'oidcservice>=0.6.3',
+        'oidcmsg>=0.6.3'
     ],
     tests_require=[
         'pytest',
