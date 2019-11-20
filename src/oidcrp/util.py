17886be0c60276e1ea12093f55b4167a4fc4a5ce
Giuseppe <giuseppe.demarco@unical.it>
2019-11-20 18:08:56 +0100

    minor change

--- src/oidcrp/util.py
+++ src/oidcrp/util.py
@@ -249,6 +249,6 @@ def load_configuration(filename):
 
 
 def do_add_ons(add_ons, services):
-        for key, spec in add_ons.items():
-            _func = importer(spec['function'])
-            _func(services, **spec['kwargs'])
+    for key, spec in add_ons.items():
+        _func = importer(spec['function'])
+        _func(services, **spec['kwargs'])
