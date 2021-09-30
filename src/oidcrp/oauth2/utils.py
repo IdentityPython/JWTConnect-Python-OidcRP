b5926ef02f6a55022691e0e9e2c73315c588374c
Roland Hedberg <roland@catalogix.se>
2021-09-30 09:58:34 +0200

    Refactoring

--- src/oidcrp/oauth2/utils.py
+++ src/oidcrp/oauth2/utils.py
@@ -25,29 +25,32 @@ def get_state_parameter(request_args, kwargs):
 
 def pick_redirect_uri(context, request_args=None):
     if 'redirect_uri' in request_args:
-        return request_args, {}
+        return request_args["redirect_uri"]
 
-    _callback = context.callback
-    if _callback:
-        _response_type = request_args.get('response_type', context.behaviour['response_types'][0])
+    if context.callback:
+        _conf_resp_types = context.behaviour.get('response_types', [])
+        _response_type = request_args.get('response_type')
+        if not _response_type and _conf_resp_types:
+            _response_type = _conf_resp_types[0]
+
         request_args['response_type'] = _response_type
 
         _response_mode = request_args.get('response_mode')
 
         if _response_mode == 'form_post':
-            request_args['redirect_uri'] = _callback['form_post']
+            redirect_uri = context.callback['form_post']
         elif _response_type == 'code':
-            request_args['redirect_uri'] = _callback['code']
+            redirect_uri = context.callback['code']
         else:
-            request_args['redirect_uri'] = _callback['implicit']
+            redirect_uri = context.callback['implicit']
 
         logger.debug(
             f"pick_redirect_uris: response_type={_response_type}, response_mode={_response_mode}, "
-            f"redirect_uri={request_args['redirect_uri']}")
+            f"redirect_uri={redirect_uri}")
     else:
-        request_args['redirect_uri'] = context.redirect_uris[0]
+        redirect_uri = context.redirect_uris[0]
 
-    return request_args, {}
+    return redirect_uri
 
 
 def pre_construct_pick_redirect_uri(request_args: Optional[Union[Message, dict]] = None,
