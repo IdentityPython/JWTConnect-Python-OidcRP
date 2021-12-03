ed00a7658b833f49170e91ca22713be3a04660f6
Giuseppe De Marco <giuseppe.demarco@unical.it>
2021-12-03 16:09:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     install_requires=[
-        'oidcmsg==1.4.1',
+        'oidcmsg>=1.5.3',
         'pyyaml>=5.1.2',
         'responses'
     ],
